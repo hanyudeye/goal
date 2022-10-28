3469d3048445a935ee98910ce35cde8df8753f68
APPLE <apple@APPLEs-MacBook-Air.local>
2022-10-29 07:34:59 +0800

    实践

--- shell/cat.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-# 文件名参数 
-argv1=sys.argv[1]
-
-fs=open(argv1,"r")
-str=fs.read()
-print(str)
--- shell/rm.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-# 文件名参数 
-argv1=sys.argv[1]
-
-os.remove(argv1)
