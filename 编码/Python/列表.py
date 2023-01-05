72c7d85035e16634d49767b50cd3e99a64e3026f
hanyudeye <hanyudeye@163.com>
2023-01-05 16:51:10 +0800

    w

---
+++ 编码/Python/列表.py
@@ -0,0 +1,9 @@
+import time
+
+偶数list=[2,4,6,8,10,12,14,16,18,20]
+
+for l in 偶数list:
+    print(l)
+    time.sleep(1)
+
+
---
+++ 编码/Python/简介.py
@@ -0,0 +1,14 @@
+
+
+class 数学:
+    def 求和(self,x,y):
+        return x+y
+
+    def 求差(self,x,y):
+        return x-y
+
+
+数学实例= 数学()
+he=数学实例.求和(10,3)
+
+print(he)
