aebda0ecb884f49267d2f092db94a61db6638d8f
aming <hanyudeye@163.com>
2023-01-25 09:38:07 +0800

    wh

---
+++ 计算机/编码操控/Java/Learn.java
@@ -0,0 +1,14 @@
+class Learn{
+
+    public static void main(String[] args) {
+
+        String greet="hello";
+        System.out.println(greet.substring(1,3));
+
+        System.out.println("你好"+"世界");
+       
+
+    }
+
+
+}
