0dc225ac3a835057228fb502b1b7f949ecde9ba2
hanyudeye <hanyudeye@163.com>
2023-02-03 17:08:26 +0800

    w

--- 编码/vue3/vue-begin/src/Page1.vue
+++ 编码/vue3/vue-begin/src/Page1.vue
@@ -1,4 +1,40 @@
 <template>
-hello,Page1
+    <h1> 标题：{{ title }} </h1>
+    <p>{{ content }}</p>
+    <button @click="handle1">按钮</button>
 </template>
-<script></script>
+
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "home",
+    // data 是 vue2 中方法 
+    // data() {
+    // return {
+    //     title: "我是标题",
+    //     content:"lorem lorem"
+    // },
+
+    // },
+    methods:{
+        handle(){
+            console.log("handle1");
+        }
+    },
+    setup() {
+
+        const handle1 = () => {
+            console.log("handle");
+        };
+
+        //这里是数据
+        return {
+            title: "我是标题",
+            content: "lorem lorem",
+            handle1:handle1,
+        }
+    }
+})
+</script>
