7a3d95959c97559148be7c0b1bf25c1c56d2144d
hanyudeye <hanyudeye@163.com>
2023-07-12 17:20:54 +0800

    w

--- Python/hello.py
+++ Python/hello.py
@@ -2,9 +2,9 @@
 
 count=10
 
-while count>0:
-    print("{0:0.1f}".format(count))
-    count-=1
+# while count>0:
+#     print("{0:0.1f}".format(count))
+#     count-=1
 
 # if 3>2:
     # print("good")
@@ -12,9 +12,16 @@ while count>0:
 # if "hello.exe" in ["hello.exe","hello.jpg"]:
     # print("nice")
 
-f=open("计算.py")
-line=f.readline()
-while line:
-    print(line)
-    line=f.readline()
+# f=open("计算.py")
+# line=f.readline()
+# while line:
+#     print(line)
+#     line=f.readline()
+
+# 打印9*9乘法表
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i,"*",j,"=",i*j,end=" ")
+    print()
 
