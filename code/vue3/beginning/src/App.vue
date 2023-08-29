e5549f4463b9dc9a9a33d8f2a954c622d16fb52f
hanyudeye <hanyudeye@163.com>
2023-08-29 17:18:58 +0800

    w

---
+++ code/vue3/beginning/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <FirstPage></FirstPage>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import FirstPage from './components/FirstPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: aquamarine;
+}
+</style>
---
+++ code/vue3/beginning/src/components/SecondPage.vue
@@ -0,0 +1,9 @@
+<div>
+    这是 page2
+</div>
+
+<script>
+export default {
+    name: "SecondPage"
+}
+</script>
