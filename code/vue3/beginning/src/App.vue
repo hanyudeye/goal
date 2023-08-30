3b20d7aff53a74443067a887a0a89e3bee584765
hanyudeye <hanyudeye@163.com>
2023-08-30 16:44:23 +0800

    w

--- code/vue3/beginning/src/App.vue
+++ code/vue3/beginning/src/App.vue
@@ -2,24 +2,27 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <FirstPage></FirstPage>
   <SecondPage></SecondPage>
+  <VueTemplate></VueTemplate>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import FirstPage from './components/FirstPage.vue'
-import SecondPage from './components/SecondPage.vue';
+// import SecondPage from './components/SecondPage.vue';
+import VueTemplate from './components/VueTemplate.vue';
 
 export default {
   name: 'App',
   components: {
     FirstPage,
-    SecondPage
+    // SecondPage,
+    VueTemplate
   }
 }
 </script>
 
 <style>
-#app {
+#app1 {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
