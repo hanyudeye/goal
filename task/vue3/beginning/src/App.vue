a37c7cee464fb86d3a32e680c74293a4306593b6
hanyudeye <hanyudeye@163.com>
2023-10-10 17:28:37 +0800

    m

--- task/vue3/beginning/src/App.vue
+++ task/vue3/beginning/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <FirstPage></FirstPage>
-  <SecondPage></SecondPage>
-  <VueTemplate></VueTemplate>
+  <!-- <FirstPage></FirstPage> -->
+  <!-- <SecondPage></SecondPage> -->
+  <!-- <VueTemplate></VueTemplate> -->
+  <KeyBoard></KeyBoard>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import FirstPage from './components/FirstPage.vue'
+// import FirstPage from './components/FirstPage.vue'
 // import SecondPage from './components/SecondPage.vue';
-import VueTemplate from './components/VueTemplate.vue';
+// import VueTemplate from './components/VueTemplate.vue';
+import KeyBoard from './components/KeyBoard.vue';
 
 export default {
   name: 'App',
   components: {
-    FirstPage,
+    // FirstPage,
     // SecondPage,
-    VueTemplate
+    // VueTemplate
+    KeyBoard
   }
 }
 </script>
