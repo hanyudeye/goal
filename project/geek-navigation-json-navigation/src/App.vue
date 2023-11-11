5136f6945284dccba9c33bfc6a8c202701074590
aming <hanyudeye@163.com>
2023-11-11 19:15:17 +0800

    w

--- project/geek-navigation-json-navigation/src/App.vue
+++ project/geek-navigation-json-navigation/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <Test></Test>
   </div>
 </template>
 
 <script>
+import Test from './components/Test.vue';
+
 
 export default {
   name: 'app',
   components: {
-  }
+    Test
+}
 }
 </script>
 
