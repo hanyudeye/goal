6320a3ed52f5900c4956c286897c86887bd87155
aming <hanyudeye@163.com>
2023-12-22 16:17:53 +0800

    w

--- cheatsheets备忘单/backend/node.js
+++ cheatsheets备忘单/backend/node.js
@@ -3,11 +3,11 @@
  * http://nodejs.org/api/synopsis.html
  * ******************************************************************************************* */
 
-
 var http = require('http');
 
 // An example of a web server written with Node which responds with 'Hello World'.
 // To run the server, put the code into a file called example.js and execute it with the node program.
+//一个 web服务器例子，以 "Hello World" 作为响应
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
