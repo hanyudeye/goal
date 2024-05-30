784d5dd8844d6f40580e0b201aabbabc8c01b9e4
hanyudeye <hanyudeye@163.com>
2024-05-30 17:29:22 +0800

    万

--- code/python/hello.py
+++ code/python/hello.py
@@ -1,11 +1,1 @@
-" o: open in prev window
-" go: preview
-" t: open in new tab
-" T: open in new tab silently
-" middle-click,
-" i: open split
-" gi: preview split
-" s: open vsplit
-" gs: preview vsplit
-" <CR>: custom open
-"
+print("hello")
