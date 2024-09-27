aa7871936818a6eb0ee9ba0986d1368c3eacbed0
aming <hanyudeye@163.com>
2024-09-27 17:34:48 +0800

    w

---
+++ it资讯/computer-language/javascript.js
@@ -0,0 +1,24 @@
+// a=[]
+// a[0]=323
+// a[1]="1abc"
+// a.forEach((element,index,w)=> {
+//    console.log(element,index,w) 
+// });
+
+
+let 小兔子="小兔子"
+小兔子+=" 笨笨"
+
+
+// for(let i=0;i<小兔子.length;i++){
+//    setTimeout(()=>{
+//       console.log(小兔子[i])
+//    },1000 *(i+1))
+// }
+
+let countries=['USA','US',"AUS","UK","CHINA","JAPAN","KOREA","INDIA","TAIWAN","HONGKONG","MACAO"]
+
+countries.push('NEWZLAND')
+// countries= countries.join(',')
+
+console.log(countries)
