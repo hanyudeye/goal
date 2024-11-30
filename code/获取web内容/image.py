92d2e4248b5eb7f92c7ff4b1f4b0befb63e40c21
aming <hanyudeye@163.com>
2024-11-30 16:48:02 +0800

    w

---
+++ code/获取web内容/image.py
@@ -0,0 +1,16 @@
+import random
+
+meizi_headers=[
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+        "a",
+        "b"
+
+]
+
+global headers
+
+headers={'User-Agent':random.choice(meizi_headers)}
+
+
+# print(dir(random))
+print(random.)
