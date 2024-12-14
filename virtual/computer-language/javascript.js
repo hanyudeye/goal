6e78432feb5f0c1e7208bc8515ed36640e2213e3
aming <hanyudeye@163.com>
2024-12-14 16:25:16 +0800

    w

--- virtual/computer-language/javascript.js
+++ virtual/computer-language/javascript.js
@@ -1,3 +1,6 @@
+// 六种基本数据类型：Undefined，Null，Boolean，Number，String，Symbol ( ES 6新增 )；
+// 一种引用数据类型：统称为 Object 类型；具体又细分为 Object，Array，Date，RegExp，Function 等类型。另外和 Java 语言类似，对于布尔，数值，字符串等基本类型，分别存在其对应的包装类型 Boolean，Number，String，但通常我们并不会使用到这些包装类型，只需要使用其基本类型即可。
+
 // a=[]
 // a[0]=323
 // a[1]="1abc"
@@ -23,6 +26,14 @@ countries.push('NEWZLAND')
 
 // console.log(countries)
 
-countries.forEach((element, index) => {
-    console.log(element)
-})
+// countries.forEach((element, index) => {
+//     console.log(element)
+// })
+
+
+// 数字的进制表示;进制显示
+// console.log(56);
+// console.log(0o70);
+// console.log(0x38);
+
+设置 - 特色功能 - 极简模式
