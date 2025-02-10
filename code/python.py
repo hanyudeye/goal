b04795bf0d76958caa4a8778e6e121c0f46f6121
阿明 <1191552161@qq.com>
2025-02-11 07:50:22 +0800

    w

--- code/python.py
+++ code/python.py
@@ -141,3 +141,7 @@ filename='test'
 with open(filename,'w') as file_object:
     file_object.write('I love programming\n')
     file_object.write('I love creating new games\n')
+
+    
+# 单元测试
+import unittest
